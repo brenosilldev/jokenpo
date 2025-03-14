b31ff514891fcef23d05e07e7409df4e89e62fec
breno-spartacode <109807534+brenosilldev@users.noreply.github.com>
2025-03-13 23:45:58 -0300

    ✨  feat: Configuração de rotas.

---
+++ src/routes.tsx
@@ -0,0 +1,25 @@
+
+import { Routes, Route } from "react-router-dom"
+import { Game } from "./pages/game"
+import { Play } from "./pages/play"
+
+const Rotas = () =>{
+
+    return(
+        <>
+            <Routes>
+               
+                <Route  path="/" element={<Play />} />
+                <Route  path="game" element={<Game />} />
+    
+         
+                
+            </Routes>
+            
+        
+        
+        </>
+    )
+}
+
+export  {Rotas};
