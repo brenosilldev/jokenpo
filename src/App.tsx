234590206df57b31402d34dcd7a899115e992338
breno-spartacode <109807534+brenosilldev@users.noreply.github.com>
2025-03-13 23:42:12 -0300

    ✨  feat: Pagina inicial criada de configuração

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+
+
+import {Rotas} from "./routes.tsx"
+import { Header } from "./componentes/header.tsx";
+
+function App() {
+
+  
+  return (
+    <>
+
+      <Header />
+      <Rotas/>
+
+      
+    </>
+  );
+}
+
+export default App;
