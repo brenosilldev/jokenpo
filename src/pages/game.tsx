897344a6d6dee2ba9165443abeb0f724ee7745a9
breno-spartacode <109807534+brenosilldev@users.noreply.github.com>
2025-03-13 23:44:17 -0300

    ✨  feat: Logica do jogo e retornano valores de estado

---
+++ src/pages/game.tsx
@@ -0,0 +1,55 @@
+
+
+
+
+
+import { useEffect } from "react";
+import { useJokenpo } from "../context/jogo";
+import { useNavigate } from "react-router-dom";
+
+
+export const Game = () => {
+    const { escolha, setScore, setScore2, setWin, score1, score2 } = useJokenpo();
+    const jokenpo = ["pedra", "papel", "tesoura"];
+    const aleatorio = jokenpo[Math.floor(Math.random() * jokenpo.length)];
+    const navigate = useNavigate();
+    const ResultadoJogo = async () => {
+      if (
+          (escolha === "pedra" && aleatorio === "tesoura") ||
+          (escolha === "tesoura" && aleatorio === "papel") ||
+          (escolha === "papel" && aleatorio === "pedra")
+      ) {
+          await setScore(score1 + 1);
+          await setWin("Você ganhou!");
+      } else if (escolha === aleatorio) {
+          await setWin("Empate!");
+      } else {
+          setScore2(score2 + 1);
+          await setWin("Máquina ganhou!");
+      }
+
+      return 1;
+  };
+
+    useEffect(() => {
+      ResultadoJogo();
+   
+    }, []);
+
+    const Zerarwin = async () => {
+        await setWin("");
+        navigate('/');
+    };
+
+    return (
+        <>
+            <div className="h-screen">
+                <div className="flex items-center justify-center mt-30">
+                  <button onClick={Zerarwin}>
+                      Play again
+                  </button>
+                </div>
+            </div>
+        </>
+    );
+}
