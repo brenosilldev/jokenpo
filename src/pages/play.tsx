150228a90a7f4cb2d4366de0dd9bf578a0d5afe2
breno-spartacode <109807534+brenosilldev@users.noreply.github.com>
2025-04-24 02:03:19 -0300

    chore: Alterado imagem para icones

--- src/pages/play.tsx
+++ src/pages/play.tsx
@@ -1,47 +1,40 @@
-
-
 import { useNavigate } from "react-router-dom"
-import Pedra from "../assets/gesto-de-punho-mão-fechada-símbolo-ilustração-do-vetor-ícone-contorno-braço-193651843.webp"
-import Tesoura from "../assets/png-transparent-computer-icons-scissors-hand-scissors-text-hand-technic.png"
-import Papel from "../assets/rock-paper-scissors-296855_1280.webp"
 import { useJokenpo } from "../context/jogo"
+import { FaScissors } from "react-icons/fa6";
+import { FaHandPaper } from "react-icons/fa";
+import { FaHandRock  } from "react-icons/fa";
 export const Play = () =>{
 
     const { setEscolha } = useJokenpo();
     const navigate = useNavigate();
 
     const jokenpo = [
-        {id : 1,escolha : 'pedra' , imagem : Pedra},
-        {id : 2,escolha : 'papel' , imagem : Papel},
-        {id : 3,escolha : 'tesoura' , imagem : Tesoura},
+        {id : 1,escolha : 'pedra' , icone :  <FaHandRock  size={38} color="black"/>},
+        {id : 2,escolha : 'papel' , icone : <FaHandPaper size={38}/>},
+        {id : 3,escolha : 'tesoura' , icone : <FaScissors size={38}/>},
     ];
 
     const handleEscolha = async (escolha:string) =>{
-
-      
         await setEscolha(escolha)
         navigate('/game');
     }
 
     return (
         <>
-            <div className="flex items-center justify-center mt-30">
-            
+            <div className="h-full flex items-center justify-center mt-30">            
                 {jokenpo.map((item) => (
-                    <div className="py-10" key={item.id}>
-                        <button onClick={() => handleEscolha(item.escolha)}>
-                            <img src={item.imagem} alt={item.escolha} className="w-30 h-30"/>    
+                    <div className="py-10 m-10 " key={item.id}>
+                        <button onClick={() => handleEscolha(item.escolha)} >
+                            <span className="w-24 h-24 flex items-center justify-center bg-gray-200 rounded-full shadow-lg hover:shadow-xl transition duration-300 ease-in-out transform hover:scale-105">
+                                {item.icone}
+                            
+                            </span>    
 
                         </button>
 
                     </div>
                 ))}
-                
-             
-                
             </div>
-
-        
         </>
 
     )
