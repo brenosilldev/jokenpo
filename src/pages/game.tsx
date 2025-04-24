6e284bd411fa61de2be521a45628b7539f7bc420
breno-spartacode <109807534+brenosilldev@users.noreply.github.com>
2025-04-24 02:04:50 -0300

    chore: Alterado css

--- src/pages/game.tsx
+++ src/pages/game.tsx
@@ -13,23 +13,24 @@ export const Game = () => {
     const jokenpo = ["pedra", "papel", "tesoura"];
     const aleatorio = jokenpo[Math.floor(Math.random() * jokenpo.length)];
     const navigate = useNavigate();
+    
     const ResultadoJogo = async () => {
-      if (
-          (escolha === "pedra" && aleatorio === "tesoura") ||
-          (escolha === "tesoura" && aleatorio === "papel") ||
-          (escolha === "papel" && aleatorio === "pedra")
-      ) {
-          await setScore(score1 + 1);
-          await setWin("Você ganhou!");
-      } else if (escolha === aleatorio) {
-          await setWin("Empate!");
-      } else {
-          setScore2(score2 + 1);
-          await setWin("Máquina ganhou!");
-      }
+        if (
+            (escolha === "pedra" && aleatorio === "tesoura") ||
+            (escolha === "tesoura" && aleatorio === "papel") ||
+            (escolha === "papel" && aleatorio === "pedra")
+        ) {
+            await setScore(score1 + 1);
+            await setWin("Você ganhou!");
+        } else if (escolha === aleatorio) {
+            await setWin("Empate!");
+        } else {
+            setScore2(score2 + 1);
+            await setWin("Máquina ganhou!");
+        }
 
-      return 1;
-  };
+        return 1;
+    };
 
     useEffect(() => {
       ResultadoJogo();
@@ -43,9 +44,9 @@ export const Game = () => {
 
     return (
         <>
-            <div className="h-screen">
-                <div className="flex items-center justify-center mt-30">
-                  <button onClick={Zerarwin}>
+            <div className="h-full flex items-center justify-center mt-30">
+                <div className="flex items-center justify-center">
+                  <button onClick={Zerarwin} className="bg-blue-500 text-white font-bold py-2 px-4 rounded">
                       Play again
                   </button>
                 </div>
