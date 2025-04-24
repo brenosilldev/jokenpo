92a652f2b939d204959ad261251041cc0fcc366b
breno-spartacode <109807534+brenosilldev@users.noreply.github.com>
2025-04-24 02:04:06 -0300

    chore: Alterado css para mobile

--- src/componentes/header.tsx
+++ src/componentes/header.tsx
@@ -4,14 +4,12 @@ export const Header = ()  =>{
     const { score1,score2,win } = useJokenpo();
     return (
         <>
-            <div className="bg-blue-500 py-10 px-20">
+            <div className="bg-blue-500 p-6">
                 <div className="grid grid-cols-1 gap-4">
-                    <div className="col-span-12 block   md:flex justify-between items-center">                        
+                    <div className="col-span-12 block   md:flex md:justify-between md:items-center">                        
                         <h1 className="font-bold text-2xl text-white">QUEM GANHOU: {win} </h1>                            
-                            <h1 className="text-white text-2xl">SEUS PONTOS: {score1}</h1> 
-                            <h1 className="text-white text-2xl">MÁQUINA: {score2} </h1> 
-                           
-
+                        <h1 className="text-white text-2xl">SEUS PONTOS: {score1}</h1> 
+                        <h1 className="text-white text-2xl">MÁQUINA: {score2} </h1> 
                     </div>
                     
                 </div>
